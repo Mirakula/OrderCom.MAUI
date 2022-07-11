75564fb74b379d1c67e5b845de88c2b8f304da3e
Mirakula <amar.dzanan@edu.fit.ba>
2022-07-11 18:01:46 +0200

    Dodana tabela stavki instdat

---
+++ OrderCom/Models/instdat.cs
@@ -0,0 +1,33 @@
+﻿using SQLite;
+
+namespace OrderCom.Models
+{
+    [Table("instdat")]
+    public class instdat
+    {
+        [Column("kn_redbrj")]
+        public double kn_redbrj { get; set; }
+        [Column("kn_sifmat")]
+        public int kn_sifmat { get; set; }
+        [Column("kn_kolmat")]
+        public double kn_kolmat { get; set; }
+        [Column("kn_cijmat")]
+        public double kn_cijmat { get; set; }
+        [Column("kn_sifpro")]
+        public string kn_sifpro { get; set; }
+        [Column("kn_brjcij")]
+        public double kn_brjcij { get; set; }
+        [Column("kn_tipmts ")]
+        public double kn_tipmts { get; set; }
+        [Column("kn_brjdok")]
+        public string kn_brjdok { get; set; }
+        [Column("kn_staslg")]
+        public string kn_staslg { get; set; }
+        [Column("kn_rabsta")]
+        public double kn_rabsta { get; set; }
+        [Column("kn_siflok")]
+        public string kn_siflok { get; set; }
+        [Column("kn_rabkup")]
+        public double kn_rabkup { get; set; }
+    }
+}
